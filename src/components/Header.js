b2fdef1f2acb420f8b6ef9bc04ac07a8c6b681de
Olzhas <11olzhas.k@gmail.com>
2021-07-19 20:29:49 +1200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../style/header.css'
+import { FiMail } from "react-icons/fi";
+import { FaGithub } from 'react-icons/fa'
+import { VscMail } from 'react-icons/vsc'
+
+
+
+function Header() {
+    return (
+        <div className="header_main">
+            <div className="header_container">
+                <div className="header_left">Logo</div>
+                  <ul className='header_links'>
+                      <li>Home</li>
+                      <li >< FaGithub className='header_git'/> Github </li>
+                      <li><VscMail className="header_mail"/></li>
+                  </ul>          
+            </div>
+        </div>
+    )
+}
+
+export default Header
