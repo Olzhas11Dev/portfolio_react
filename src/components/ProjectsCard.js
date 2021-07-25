5de895576c3cd54747b55efa5964e16cbd0dfe41
Olzhas <11olzhas.k@gmail.com>
2021-07-26 02:39:50 +1200

    before responsive

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -4,6 +4,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai'
 
 function ProjectsCard(props) {
     const[modal,setModal] = useState(false)
+   const [path,setPath] = useState()
 
     let modalBlock = 
     <div className="project_modal" > 
@@ -17,17 +18,28 @@ function ProjectsCard(props) {
                 omnis ad quis sequi repudiandae recusandae exercitationem 
                 sunt tempore quidem accusamus laborum, similique sit?
                 </div>
-                <button className="form_btn"> Go to project</button>
+               <a  target="_blank" rel="noreferrer" href={path}>  <button onClick={()=>openPage(props.id)} className="form_btn">Go to project  </button></a>
             </div>
             <AiOutlineCloseCircle onClick={()=>setModal(false)} className="project_close_btn" />
         </div>
     </div>
 
+    function openPage (id){
+        console.log(id)
+        if(id===0){
+            setPath('https://mystifying-nightingale-3e6063.netlify.app')
+        } else if (id===1){
+            setPath('https://ol11-weather.netlify.app')
+        } else if (id===2){
+            setPath('https://relaxed-ptolemy-b1d863.netlify.app')
+        }
+    }
+
     function makeDec (){
         console.log( ' you clicked ' + props.title  )
         setModal(true)
     }
-    console.log(modal)
+    console.log()
     return (
         <div   className="projects_card">    
             <div onClick={()=>makeDec(props.id)} >         
